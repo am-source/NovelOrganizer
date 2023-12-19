6abd61c26d87e7bb7c0cdb6f53a25fa6f2d9250c
am-source <a.mardini@gmx.net>
2023-12-19 12:28:41 +0100

    Setup routing for project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import routes from './routes';
+import VueRouter from 'vue-router';
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+Vue.config.productionTip = false;
+
+const router = new VueRouter({
+  routes
+});
+
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  router
+}).$mount('#app');
---
+++ src/routes.js
@@ -0,0 +1,5 @@
+import HelloWorld from './components/HelloWorld'
+
+export default [
+    { path: '/', component: HelloWorld},
+]
